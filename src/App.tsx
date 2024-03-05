914607f4f291647ef677ce389c7955acd30b6273
ariefwic <ariefw904@gmail.com>
2024-03-05 15:13:59 +0700

    :potoforlio

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+      <div className="bg-blue-500 md:bg-green-500 p-5 flex justify-center items-center h-screen">
+          <div className="bg-green-100 w-40 h-40 rounded-[50%]">
+              UHUUYY
+          </div>
+      </div>
+  ) 
+  }
+export default App
+
